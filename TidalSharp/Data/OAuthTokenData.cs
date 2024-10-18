ce16673d7227fb47b0651b21c5378c246ff7670b
Trevor <me@trev.app>
2024-10-17 23:35:36 -0400

    Remove unnecessary info from OAuthTokenData
    Seems like an unnecessary additional security risk to be storing these in plain text (ignoring the fact we are storing auth tokens in plain text too)

--- TidalSharp/Data/OAuthTokenData.cs
+++ TidalSharp/Data/OAuthTokenData.cs
@@ -34,77 +34,8 @@ public class OAuthTokenData
         [JsonProperty("userId")]
         public long UserId { get; set; }
 
-        [JsonProperty("email")]
-        public string Email { get; set; }
-
         [JsonProperty("countryCode")]
         public string CountryCode { get; set; }
-
-        [JsonProperty("fullName")]
-        public object FullName { get; set; }
-
-        [JsonProperty("firstName")]
-        public object FirstName { get; set; }
-
-        [JsonProperty("lastName")]
-        public object LastName { get; set; }
-
-        [JsonProperty("nickname")]
-        public object Nickname { get; set; }
-
-        [JsonProperty("username")]
-        public string Username { get; set; }
-
-        [JsonProperty("address")]
-        public object Address { get; set; }
-
-        [JsonProperty("city")]
-        public object City { get; set; }
-
-        [JsonProperty("postalcode")]
-        public object Postalcode { get; set; }
-
-        [JsonProperty("usState")]
-        public object UsState { get; set; }
-
-        [JsonProperty("phoneNumber")]
-        public object PhoneNumber { get; set; }
-
-        [JsonProperty("birthday")]
-        public object Birthday { get; set; }
-
-        [JsonProperty("channelId")]
-        public long ChannelId { get; set; }
-
-        [JsonProperty("parentId")]
-        public long ParentId { get; set; }
-
-        [JsonProperty("acceptedEULA")]
-        public bool AcceptedEula { get; set; }
-
-        [JsonProperty("created")]
-        public long Created { get; set; }
-
-        [JsonProperty("updated")]
-        public long Updated { get; set; }
-
-        [JsonProperty("facebookUid")]
-        public long FacebookUid { get; set; }
-
-        [JsonProperty("appleUid")]
-        public object AppleUid { get; set; }
-
-        [JsonProperty("googleUid")]
-        public object GoogleUid { get; set; }
-
-        [JsonProperty("accountLinkCreated")]
-        public bool AccountLinkCreated { get; set; }
-
-        [JsonProperty("emailVerified")]
-        public bool EmailVerified { get; set; }
-
-        [JsonProperty("newUser")]
-        public bool NewUser { get; set; }
     }
 }
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
